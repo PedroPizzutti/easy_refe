ff235d723b5c47add2c252e7acff5f935a5bb364
Pedro Pizzutti <pedropizzutti@gmail.com>
2022-01-08 12:08:37 -0300

    criado o projeto

---
+++ src/main/java/io/github/pedropizzutti/acervo_referencias/AcervoReferenciasApplication.java
@@ -0,0 +1,14 @@
+package io.github.pedropizzutti.acervo_referencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervoReferenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcervoReferenciasApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/io/github/pedropizzutti/acervo_referencias/AcervoReferenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedropizzutti.acervo_referencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoReferenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
