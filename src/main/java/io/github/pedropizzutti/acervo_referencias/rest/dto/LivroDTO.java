fb780c124790ef352dbade03096a31fc3411ff07
Pedro Pizzutti <pedropizzutti@gmail.com>
2022-01-18 10:28:07 -0300

    implementada as validações para LivroDTO

--- src/main/java/io/github/pedropizzutti/acervo_referencias/rest/dto/LivroDTO.java
+++ src/main/java/io/github/pedropizzutti/acervo_referencias/rest/dto/LivroDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -16,23 +17,25 @@ public class LivroDTO {
 
     private Integer idRegistro;
 
-    @NotNull
+    @NotNull(message = "{campo.livro.usuario.obrigatorio}")
     private Integer idUsuario;
 
-    @NotEmpty
-    @Length(min = 4, max = 100)
+    @NotEmpty(message = "{campo.livro.autor.obrigatorio}")
+    @Length(min = 4, max = 100, message = "{campo.livro.autor.comprimento}")
     private String autor;
 
-    @NotEmpty
-    @Length(min = 4, max = 200)
+    @NotEmpty(message = "{campo.livro.titulo.obrigatorio}")
+    @Length(min = 4, max = 200, message = "{campo.livro.titulo.comprimento}")
     private String titulo;
 
+    @Max(value = 9999, message = "{campo.livro.ano.comprimento}")
     private Integer ano;
 
-    @NotEmpty
-    @Length(min = 4, max = 300)
+    @NotEmpty(message = "{campo.livro.referencia.obrigatorio}")
+    @Length(min = 30, max = 300, message = "{campo.livro.referencia.comprimento}")
     private String referencia;
 
+    @Size(max = 300, message = "{campo.livro.anotacao.comprimento}")
     private String anotacao;
 
 }
